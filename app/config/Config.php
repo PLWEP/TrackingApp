9d42ee0ecab1bbdc2e076d1562a8fa4a23f76e93
PLWEP <langgeng86@gmail.com>
2022-07-12 22:17:36 +0700

    base url update

--- app/config/Config.php
+++ app/config/Config.php
@@ -1,5 +1,5 @@
 <?php 
-    define('BASEURL', 'http://localhost/TrackingApp/public');
+    define('BASEURL', 'https://dapper-dasik-3e1cd8.netlify.app/public');
     define('API_URL', 'https://api-eu.dhl.com/track/shipments?');
     define('API_Key', '9B9gqG019ULC7472gYvFO7gCdz7Q9n5f');
     define('API_Secret', 'r34WGZatP0aVPJ3i');
