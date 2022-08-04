8132edc890877942a10a60a1813c356bc69bee5f
PLWEP <76485662+PLWEP@users.noreply.github.com>
2022-08-04 19:10:31 +0700

    change api (#1)

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,9 +1,15 @@
 <main>
     <div class="wrapper">
         <div class="main-header">
-            <form action="<?=BASEURL;?>/data/addData" method="POST">
-                <label for="iresi">Input Tracking Number</label>
-                <input type="text" name="iresi" id="iresi" minlength="10" maxlength="10">
+            <form action="<?=BASEURL;?>data/addData" method="POST">
+                <label for="tracking_number">Input Tracking Number</label>
+                <input type="text" name="tracking_number" id="tracking_number">
+                <label for="carrier">Select Carrier</label>
+                <select name="carrier" id="carrier">
+                    <option value="ups">UPS</option>
+                    <option value="fedex">FedEx</option>
+                    <option value="dhl">DHL</option>
+                </select>
                 <label for="name">Input Name</label>
                 <input type="text" name="name" id="name">
                 <button>Add</button>
@@ -12,27 +18,25 @@
         <div class="main-content">
             <table>
                 <tr>
-                    <th>Name</th>
+                    <th>Title</th>
                     <th>Tracking Number</th>
-                    <th>Service</th>
-                    <th>Location</th>
+                    <th>Carrier</th>
                     <th>Status</th>
-                    <th>Description</th>
-                    <th>Timestamp</th>
+                    <th>Last Update</th>
+                    <th>Last Event</th>
                     <th>Action</th>
                 </tr>
                 <?php 
                 if(isset($data["trackingNumberdata"])) :
                     foreach($data["trackingNumberdata"] as $data) :?>
                 <tr>
-                    <td><?= $data['name'] ?></td>
+                    <td><?= $data['title'] ?></td>
                     <td><?= $data['trackingNumber'] ?></td>
-                    <td><?= $data['service'] ?></td>
-                    <td><?= $data['address'] ?></td>
+                    <td><?= $data['carrier_code'] ?></td>
                     <td><?= $data['status'] ?></td>
-                    <td><?= $data['description'] ?></td>
-                    <td><?= $data['timestamp'] ?></td>
-                    <td><a href="<?= BASEURL; ?>/data/deleteData/<?= $data['trackingNumber'];?>" class="btn">Delete</a></td>
+                    <td><?= $data['lastupdate'] ?></td>
+                    <td><?= $data['lastevent'] ?></td>
+                    <td><a href="<?= BASEURL; ?>data/deleteData/<?= $data['id'];?>" class="btn">Delete</a></td>
                 </tr>
                 <?php 
                     endforeach; 
