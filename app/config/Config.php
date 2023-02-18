7f6ecc55b736362911d1aeea196f35305efef48c
PLWEP <76485662+PLWEP@users.noreply.github.com>
2023-02-18 14:06:57 +0700

    Update Config.php

--- app/config/Config.php
+++ app/config/Config.php
@@ -1,7 +1,7 @@
 <?php 
     define('BASEURL', 'http://localhost/TrackingApp/public/');
     define('API_URL', 'https://api.tracktry.com/v1/trackings/');
-    define('API_Key', '9987844c-bf07-4c0d-bb44-249b0a9bd40c');
+    define('API_Key', '-');
     define('DB_PASS', '');
     define('DB_USER', 'root');
     define('DB_NAME', 'track');
